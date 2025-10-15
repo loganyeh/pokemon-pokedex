36fddc29602729b16812612a6bbcb4dc88c6c2bc
loganyeh <yehlogan@gmail.com>
2025-10-15 13:43:20 -0400

    dom elements and event listeners added

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// DOM ELEMENTS
+const inputSearchBar = document.getElementById("input-search-bar")
+const searchButton = document.getElementById("search-button");
+
+// VARIABLES
+
+// EVENT LISTENERS
+inputSearchBar.addEventListener("click", () => {
+    console.log("input search bar clicked");
+});
+
+searchButton.addEventListener("click", () => {
+    console.log("search button clicked");
+});
+
