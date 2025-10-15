f68e36ade1c41e5d65fc84678843fefbd051b9eb
loganyeh <yehlogan@gmail.com>
2025-10-15 14:28:52 -0400

    fetched api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inputSearchBar = document.getElementById("input-search-bar")
 const searchButton = document.getElementById("search-button");
 
 // VARIABLES
+const pokemonAPI = "https://pokeapi.co/";
 
 // EVENT LISTENERS
 inputSearchBar.addEventListener("click", () => {
@@ -13,4 +14,28 @@ inputSearchBar.addEventListener("click", () => {
 searchButton.addEventListener("click", () => {
     console.log("search button clicked");
 });
+
+// API FETCH FUNCTION REQUEST
+// VERSION 1 to FETCH AN API
+// fetch("https://pokeapi.co/")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data.content);
+//     })
+//     .catch(error => {
+//         console.error("Error:", error);
+//     });
+
+async function getPokemon(){
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/squirtle");
+        const data = await response.json();
+        console.log(data);
+    }     
+    catch (error) {
+        console.error("Error:", error);
+    }    
+}    
+
+getPokemon();
 
