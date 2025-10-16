04a6d9422d9c1ba6339aa32420f304f9f4d7ba04
loganyeh <yehlogan@gmail.com>
2025-10-16 17:33:45 -0400

    implemented the pokemon sprite with the search

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const searchButton = document.getElementById("search-button");
 const inputSearchBar = document.getElementById("input-search-bar");
 const pokemonNameTitle = document.getElementById("pokemon-name");
 const pokemonImage = document.getElementById("pokemon-image");
+const pokemonImgElement = document.getElementById("pokemon-image-element");
 
 // VARIABLES
 const pokemonAPI = "https://pokeapi.co/";
@@ -18,34 +19,37 @@ searchButton.addEventListener("click", async () => {
     try { 
         const inputValue = document.getElementById("input-search-bar").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
-        const capitalizePokemonName = capitalize(inputValue);
-
+        
         if(!response.ok){
-            console.error(`Could not fetch resource. ${inputValue} is not a Pokemon.`);
+            console.error(`Could not fetch resource. "${inputValue}" is not a Pokemon.`);
             pokemonNameTitle.innerHTML = `
-                <h1 class="boldPokemonName">ERROR</h1>
+            <h1 class="boldPokemonName">ERROR</h1>
             `;
             setTimeout(() => {
                 pokemonNameTitle.innerHTML = `
-                    <h1>ERROR</h1>
+                <h1>ERROR</h1>
                 `;
             }, 3000);
             clearInputBar();
         };
-
+        
         if(inputValue == ""){
             pokemonNameTitle.innerHTML = `
-                <h1>Enter a Pokemon</h1>
+                <h1>Enter a Pokemon Name!</h1>
             `;            
+            pokemonImage.innerHTML = `
+                <img src="sad.jpg"/>
+            `
             // THIS RETURN KEYWORD EXITS FUNCTION 
             // SO THE CODE FOLLOWING/AFTER WILL NOT RUN
             return;
         };
-
+        
         // RESPONSE IS CONVERTED TO A JSON FILE FOR READABLE FOR THE IDE
         const data = await response.json();
+        const capitalizePokemonName = capitalize(data.name);
         console.log(`Searching for ${capitalizePokemonName}...`);
-        const pokemonName = data.name;
+        console.log(data);
         
         // LOADING POKEMON NAME FEATURE
         pokemonNameTitle.innerHTML = `
@@ -55,37 +59,43 @@ searchButton.addEventListener("click", async () => {
             pokemonNameTitle.innerHTML = `
             <h1>Searching .</h1>
         `;
-        }, 1000);
+        }, 250);
         setTimeout(() => {
             pokemonNameTitle.innerHTML = `
             <h1>Searching ..</h1>
         `;
-        }, 1500);
+        }, 500);
         setTimeout(() => {
             pokemonNameTitle.innerHTML = `
             <h1>Searching ...</h1>
         `;
-        }, 2000);
+        }, 750);
         setTimeout(() => {
             pokemonNameTitle.innerHTML = `
             <h1 class="boldPokemonName">${capitalizePokemonName}</h1>
         `;
-        }, 2500);
+            pokemonImage.innerHTML = `
+                <img src="${data.sprites.front_default}" alt="${capitalizePokemonName} image not found
+                    name: ${data.name}
+                ">
+        `;
+        }, 1000);
         setTimeout(() => {
             pokemonNameTitle.innerHTML = `
             <h1>${capitalizePokemonName}</h1>
-        `;
-        }, 5500);
-
+            `;
+        }, 2000);
+    
     }
     catch (error) {
-        console.error(`${inputValue} is not a Pokemon`);
+        //console.error(`${inputValue} is not a Pokemon`);
+        //console.error(`${error} is not a Pokemon`);
     } 
 
+    
     //console.log("search button clicked");
     clearInputBar();
     // clearPokemonNameTitle();
-
 });
 
 function clearInputBar(){
