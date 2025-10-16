a328773d79a70564376b52e6156aa0c721ce1888
loganyeh <yehlogan@gmail.com>
2025-10-16 12:22:42 -0400

    input value and api search now working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // DOM ELEMENTS
-const inputSearchBar = document.getElementById("input-search-bar")
 const searchButton = document.getElementById("search-button");
+const inputSearchBar = document.getElementById("input-search-bar");
 
 // VARIABLES
 const pokemonAPI = "https://pokeapi.co/";
@@ -11,9 +11,35 @@ inputSearchBar.addEventListener("click", () => {
     console.log("input search bar clicked");
 });
 
-searchButton.addEventListener("click", () => {
+searchButton.addEventListener("click", async () => {
+    
+    // GRABS VALUE FROM THE INPUT BAR
+    // console.log(inputValue);
+    try { 
+        const inputValue = document.getElementById("input-search-bar").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+    catch (error) {
+        console.error(`Pokemon not found`);
+    } 
+
     console.log("search button clicked");
+    // console.log(inputSearchBar.value);
+    // console.log(document.getElementById("input-search-bar").value);
+    // This is returning the value of whatever is in the search bar at the time
+    // BOTH WORK ^^
+
+    clearInputBar();
+
 });
+
+function clearInputBar(){
+    //console.log("cleared input bar");
+    document.getElementById("input-search-bar").value = "";
+};
 
 // API FETCH FUNCTION REQUEST
 // VERSION 1 to FETCH AN API
@@ -26,15 +52,3 @@ searchButton.addEventListener("click", () => {
 //         console.error("Error:", error);
 //     });
 
-async function getPokemon(){
-    try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/squirtle");
-        const data = await response.json();
-        console.log(data.name);
-    }     
-    catch (error) {
-        console.error("Error:", error);
-    }    
-}    
-
-getPokemon();
